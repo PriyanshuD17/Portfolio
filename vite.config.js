93d10ceceae72f49a0f1029dd0c8abfa95e6fb98
PriyanshuD17 <priyanshudubey172004@gmail.com>
2026-08-08 16:18:05 +0530

    Configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',
   server: {
     port: 3000,
     strictPort: true,
     host: true
   }
 })
-
